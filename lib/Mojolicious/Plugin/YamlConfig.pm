78dd3d09436cca82633b2f0cc761ece4735045f6
Danijel Tasov <dt@korn.shell.la>
2010-07-13 10:22:30 +0200

    init

---
+++ lib/Mojolicious/Plugin/YamlConfig.pm
@@ -0,0 +1,80 @@
+# vim: ai et sw=4
+# ABSTRACT: YAML Configuration Plugin for Mojolicious
+use strict;
+use warnings;
+package Mojolicious::Plugin::YamlConfig;
+
+use YAML::Any;
+use namespace::autoclean;
+use base 'Mojolicious::Plugin::JsonConfig';
+
+our $VERSION = '0.0.1';
+
+sub register {
+   my ($self, $app, $conf) = @_;
+   $conf ||= {};
+   $conf->{ext} ||= 'yaml';
+   return $self->SUPER::register($app, $conf);
+}
+
+sub _parse_config {
+    my ($self, $encoded, $name) = @_;
+    local $@;
+
+    # Parse
+    my ($config,$error);
+    eval {
+        $config = Load($encoded);
+    }; if($@) {
+        $error = $@;
+    }
+
+    die qq/Couldn't parse config "$name": $error/ if !$config && $error;
+    die qq/Invalid config "$name"./ if !$config || ref $config ne 'HASH';
+
+    return $config;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Mojolicious::Plugin::YamlConfig - YAML Configuration Plugin
+
+=head1 SYNOPSIS
+
+    # myapp.yaml
+    --
+    foo: "bar"
+    music_dir: "<%= app->home->rel_dir('music') %>"
+
+    # Mojolicious
+    $self->plugin('yaml_config');
+
+    # Mojolicious::Lite
+    plugin 'yaml_config';
+
+    # Reads myapp.yaml by default and puts the parsed version into the stash
+    my $config = $self->stash('config');
+
+    # Everything can be customized with options
+    my $config = plugin json_config => {
+        file      => '/etc/myapp.conf',
+        stash_key => 'conf'
+    };
+
+=head2 DESCRIPTION
+
+Look at L<Mojolicious::Plugin::JsonConfig> and replace "json" with "yaml"
+and you should be fine. :)
+
+=head2 AUTHOR
+
+Danijel Tasov <data@cpan.org>
+
+=head2 SEE ALSO
+
+L<Mojolicious>, L<Mojolicous::Plugin::JsonConfig>, L<Mojolicious::Guides>
+
---
+++ t/yaml_config_lite_app.t
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+# Copyright (C) 2008-2010, Sebastian Riedel.
+# Copyright (C) 2010, Danijel Tasov.
+
+use strict;
+use warnings;
+
+use utf8;
+
+# Disable epoll, kqueue and IPv6
+BEGIN { $ENV{MOJO_POLL} = $ENV{MOJO_NO_IPV6} = 1 }
+
+use Mojo::IOLoop;
+use Test::More;
+
+# Make sure sockets are working
+plan skip_all => 'working sockets required for this test!'
+  unless Mojo::IOLoop->new->generate_port;
+plan tests => 8;
+
+# Oh, I always feared he might run off like this.
+# Why, why, why didn't I break his legs?
+use Mojolicious::Lite;
+use Test::Mojo;
+
+# Silence
+app->log->level('error');
+
+# Load plugin
+my $config =
+  plugin yaml_config => {default => {foo => 'baz', hello => 'there'}};
+use Data::Dumper::Concise;
+is($config->{foo},   'bar');
+is($config->{hello}, 'there');
+is($config->{utf},   'утф');
+
+# GET /
+get '/' => 'index';
+
+my $t = Test::Mojo->new;
+
+# GET /
+$t->get_ok('/')->status_is(200)->content_like(qr/bar/);
+
+# No config file, default only
+$config =
+  plugin yaml_config => {file => 'nonexisted', default => {foo => 'qux'}};
+is($config->{foo}, 'qux');
+
+# No config file, no default
+ok(not eval { plugin yaml_config => {file => 'nonexisted'}; });
+
+__DATA__
+@@ index.html.ep
+<%= $config->{foo} %>
