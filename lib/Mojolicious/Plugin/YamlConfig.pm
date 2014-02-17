8551a0a1857329d91349b29240d9dd59041c277c
Danijel Tasov <m@rbfh.de>
2014-02-17 13:40:11 +0100

    YAML::Tiny suddenly is broken
    It expects decoded data as input

--- lib/Mojolicious/Plugin/YamlConfig.pm
+++ lib/Mojolicious/Plugin/YamlConfig.pm
@@ -5,7 +5,7 @@ package Mojolicious::Plugin::YamlConfig;
 
 use base 'Mojolicious::Plugin::JSONConfig';
 
-our $VERSION = '0.1.6';
+our $VERSION = '0.1.7';
 
 sub register {
     my ( $self, $app, $conf ) = @_;
@@ -31,7 +31,7 @@ sub parse {
     # Render
     $content = $self->render($content, $file, $conf, $app);
 
-    my @broken = qw(YAML YAML::Old);
+    my @broken = qw(YAML YAML::Old YAML::Tiny);
     if (grep { $class eq $_ } @broken) {
         # they are broken *sigh*
         $content = Encode::decode('UTF-8', $content);
