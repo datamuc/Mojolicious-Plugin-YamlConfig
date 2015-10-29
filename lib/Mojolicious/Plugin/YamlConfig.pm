adc7df4cfae679356ce831514494ba0c687e2a31
Danijel Tasov <m@rbfh.de>
2015-10-29 14:53:05 +0100

    RT #108072 - update MANIFEST.SKIP to exclude MYMETA*

--- lib/Mojolicious/Plugin/YamlConfig.pm
+++ lib/Mojolicious/Plugin/YamlConfig.pm
@@ -5,7 +5,7 @@ package Mojolicious::Plugin::YamlConfig;
 
 use base 'Mojolicious::Plugin::JSONConfig';
 
-our $VERSION = '0.2.0';
+our $VERSION = '0.2.1';
 
 sub register {
     my ( $self, $app, $conf ) = @_;
